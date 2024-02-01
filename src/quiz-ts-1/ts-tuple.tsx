6ae42051e76651c11ab696cfe070148f494738bc
Andrea Carmona <andrea@carmonaweb.com>
2024-02-01 09:45:05 -0500

    Finished ts quiz 1

--- src/quiz-ts-1/ts-tuple.tsx
+++ src/quiz-ts-1/ts-tuple.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 function TupleComponent() {
-  // Definir el estado como una tupla que contiene un booleano y un string
+ 
   const [state, setState] = React.useState<[boolean, string]>([false, '']);
 
-  // Ejemplo de función que utiliza aserción de tipo
+  
   const updateState = (newState: boolean | string) => {
     if (typeof newState === 'boolean') {
       setState([newState, state[1]]);
     } else {
-      // Aserción de tipo para tratar 'newState' como un string
+      
       setState([state[0], newState as string]);
     }
   };
